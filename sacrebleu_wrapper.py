dacd4301ac6a357452e60b5de9a7ea59e14c3baf
banyaroo26 <oohenry225@gmail.com>
2026-04-19 18:15:34 +0630

    Fix tqdm function

--- sacrebleu_wrapper.py
+++ sacrebleu_wrapper.py
@@ -18,7 +18,7 @@ class Evaluator():
         self.source = source
         refs = []
 
-        for src, ref in tqdm(zip(source, target), total=len(source)):
+        for src, ref in tqdm.tqdm(zip(source, target), total=len(source)):
             self.translations.append(self.translate_func(src))
             refs.append(ref)
         self.references.append([refs]) # sacrebleu requires list of list of references
